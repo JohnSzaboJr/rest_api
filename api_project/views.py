53e064463b3ad595726f32d323737f71cc5ca2f6
Janos SZABO <jszabo@e3r10p14.42.fr>
2018-09-08 19:58:02 +0200

    experimenting

--- api_project/views.py
+++ api_project/views.py
@@ -10,20 +10,16 @@
 #                                                                              #
 # **************************************************************************** #
 
-from django.shortcuts import render
-
 from rest_framework import generics
 from .serializers import UsersSerializer
 from .models import Users
 
 class CreateView(generics.ListCreateAPIView):
+    #handling POST
     queryset = Users.objects.all()
     serializer_class = UsersSerializer
 
-    def perform_create(self, serializer):
-        serializer.save()
-
 class DetailsView(generics.RetrieveUpdateDestroyAPIView):
-
+    #handling GET, PUT, DELETE
     queryset = Users.objects.all()
     serializer_class = UsersSerializer
