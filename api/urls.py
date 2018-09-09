2ca4366234968f49080436282487fcdc8297ca8a
Janos SZABO <jszabo@e3r10p14.42.fr>
2018-09-09 18:07:01 +0200

    main api implemented, experimenting with front-end

--- api/urls.py
+++ api/urls.py
@@ -14,12 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-#from django.urls import path, include
 from django.conf.urls import url, include
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('api_project.urls'))
-#    path('admin/', admin.site.urls),
-#    path('', include('api_project.urls'))
 ]
