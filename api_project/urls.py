302ca8e5d8d53df72de3d9d755db46976b80c576
Janos SZABO <jszabo@e3r9p9.42.fr>
2018-09-10 17:04:40 +0200

    updated user auth

--- api_project/urls.py
+++ api_project/urls.py
@@ -2,11 +2,12 @@ from django.conf.urls import url, include
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import CreateView, DetailsView
+from .views import CreateView, DetailsView, GetView
 
 urlpatterns = [
     url(r'^auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^Cities/$', CreateView.as_view(), name="create"),
+    url(r'^Cities/$', GetView.as_view(), name="cities"),
+    url(r'^Newcity/$', CreateView.as_view(), name="create"),
     url(r'^Cities/(?P<pk>[0-9]+)/$', DetailsView.as_view(), name="details"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
--- api_project/views.py
+++ api_project/views.py
@@ -1,16 +1,23 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .serializers import CitySerializer
 from .models import City
 
 class CreateView(generics.ListCreateAPIView):
-    #handling POST
+    #handling POST, GET
     queryset = City.objects.all()
     serializer_class = CitySerializer
+    permission_classes = (permissions.IsAuthenticated,)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
-class DetailsView(generics.RetrieveUpdateDestroyAPIView):
-    #handling GET, PUT, DELETE
+class GetView(generics.ListAPIView):
+    #handling GET
     queryset = City.objects.all()
     serializer_class = CitySerializer
+
+class DetailsView(generics.RetrieveUpdateDestroyAPIView):
+    #handling PUT, DELETE
+    queryset = City.objects.all()
+    serializer_class = CitySerializer
+    permission_classes = (permissions.IsAuthenticated,)
