7b5b91611d9bdf91f8fea92323df011481fc8073
John Szabo <john.szabo.jr@gmail.com>
2018-09-10 20:34:59 +0200

    update get_test

--- test_scripts/get_test.sh
+++ test_scripts/get_test.sh
@@ -2,6 +2,7 @@ YELLOW='\033[0;33m'
 GREEN='\033[0;32m'
 RESET='\033[0m'
 
+clear
 echo ${YELLOW}'Testing GET method: (list of objects)'${RESET}
 echo ${YELLOW}'-------------------------------------\n'${RESET}
 echo ${GREEN}'REQUEST:\n'${RESET}'curl -X GET\
